e1953e87cf92454315dadc5b9af6bfe8e5f95836
Daniel <daniel.fb.jr@gmail.com>
2022-08-18 22:00:10 -0300

    Primeiro programa

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Comentário = escreva o que necessitar.
+
+// Variável = espaço reservado na memória do computador.
+
+//  inteiro = int
+int a;
+
+//  Para escrever na tela = Console.Write("Texto");
+Console.Write("Digite o valor de a: ");
+
+//  Para ler um valor = Console.ReadLine();
+a = Convert.ToInt32( Console.ReadLine() ) ;
+
+Console.Write("O valor digitado é = ");
+Console.Write(a);
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Aula de C#
 
 Para iniciar o projeto:
+dotnet new console
+
+Para "rodar" ou executar um projeto:
+dotnet run
+
+
