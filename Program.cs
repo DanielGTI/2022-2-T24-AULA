1188a5533f1ac5895caecd044d1ad02d9849be66
Daniel <daniel.fb.jr@gmail.com>
2022-09-01 22:01:30 -0300

    Exercicios

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
 ﻿static void p1001(){
-    Console.Write("Prog 1");
+    int a, b, x;
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    x = a + b;
+    Console.Write("X = {0}\n", x);
 }
 
 static void p1002(){
-    Console.Write("Prog 2");
+    double area, raio;
+    raio = Convert.ToDouble( Console.ReadLine() );
+    //  area = 3.14159 * (raio * raio);
+    area = 3.14159 * Math.Pow(raio, 2.0);
+    Console.Write("A={1}, raio={0}\n", area.ToString("0.0000"), raio);
 }
   
 static void p1003(){
-    Console.Write("Prog 3");
+    int a, b, soma;
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    soma = a + b;
+    Console.Write("SOMA = {0}\n", soma);
+}
+
+static void p1005(){
+    double a, b, media;
+
+    a = Convert.ToDouble(Console.ReadLine());
+    b = Convert.ToDouble(Console.ReadLine());
+    media = (a*3.5 + b*7.5) / ( 3.5 + 7.5 );
+    Console.Write("MEDIA = {0}\n", media.ToString("0.00000"));
 }
     int prog;
     Console.Clear();
@@ -21,6 +42,7 @@ static void p1003(){
             case 1001:  p1001();    break;
             case 1002:  p1002();    break;
             case 1003:  p1003();    break;
+            case 1005:  p1005();    break;
             case 0:     Console.Write("\nObrigado por utilizar nosso Software\n");    break;
             default:    Console.Write("\nOpção Inválida\n");    break;            
         }
